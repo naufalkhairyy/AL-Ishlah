61483ee352c287bf9aab4335ac01054284bbb0bc
naufalkhairyy <naufal22si@mahasiswa.pcr.ac.id>
2026-06-27 18:50:36 +0700

    Fix CORS policy

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+   'allowed_origins' => ['https://alishlahalislamy.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
